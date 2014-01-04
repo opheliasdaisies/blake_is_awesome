be7fa32fc070d042db4c9ce612693d8e2e0e1757
nishacodes <nishabatra27@gmail.com>
2014-01-04 16:14:32 -0500

    create handlers and views for CRUD compliments

--- app.rb
+++ app.rb
@@ -7,9 +7,64 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/complime
 module Blake
   class Awesome < Sinatra::Application
     
+    # MAIN PAGE
     get '/' do
       @compliments = Compliment.all
       erb :index
+    end
+
+    # ROUTES FOR CRUD COMPLIMENTS
+    get '/compliments' do
+      @compliments = Compliment.all
+      erb :crud/show
+    end
+
+    # page for new compliment
+    get '/compliments/new' do
+      @compliment = Compliment.new
+      erb :new
+    end
+
+    # create new compliment 
+    post '/compliments' do
+      @compliment = Compliment.new(params[:compliment])
+      if @compliment.save
+        # status 201
+        redirect '/compliments'
+      else
+        # status 400
+        erb :new
+      end
+    end
+
+    # edit compliment
+    get '/compliments/edit/:id' do
+      @compliment = Compliment.get(params[:id])
+      erb :edit
+    end
+
+    # update compliment
+    put '/compliments/:id' do
+      @compliment = Compliment.get(params[:id])
+      if @compliment.update(params[:compliment])
+        # status 201
+        redirect '/compliments'
+      else
+        # status 400
+        erb :edit  
+      end
+    end
+
+    # delete compliment confirmation
+    get '/compliments/delete/:id' do
+      @compliment = Compliment.get(params[:id])
+      erb :delete
+    end
+
+    # delete compliment
+    delete '/compliments/:id' do
+      Compliment.get(params[:id]).destroy
+      redirect '/compliments'  
     end
 
     helpers do 
